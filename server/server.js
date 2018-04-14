a220d8ee4576cb7312786ade7820b896279fc337
Karen MacPherson <karen@fullstackacademy.com>
2018-04-14 09:18:59 -0400

    made some changes--it should work now

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //starts the server and listens for requests
 const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
